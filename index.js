3770c37f5f91ad01c95272559a02cd7cc20e8859
Chieke Emeka <mekus1085@gmail.com>
2023-09-30 19:04:26 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".fa-bars");
+const show = document.querySelector("ul");
+const close = document.querySelector(".fa-solid");
+
+const opener = function () {
+  show.classList.remove("hidden");
+  hamburger.classList.add("hidden");
+  hamburger.classList.remove("fa-bars");
+  hamburger.classList.remove("fa-solid");
+  close.classList.remove("hidden");
+  close.classList.add("fa-xmark");
+  close.classList.add("fa-solid");
+  console.log(opener);
+};
+
+hamburger.addEventListener("click", opener);
+
+close.addEventListener("click", function () {
+  console.log("cancel");
+});
