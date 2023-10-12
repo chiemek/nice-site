97e4c23e5a2ffd056af1e76cb595106a4581c733
Chieke Emeka <mekus1085@gmail.com>
2023-10-12 21:49:53 +0100

    last touching

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// const hamburger = document.querySelector(".fa-bars");
-// const show = document.querySelector("ul");
-// const close = document.querySelector(".fa-solid");
-
-// const opener = function () {
-//   show.classList.remove("hidden");
-//   hamburger.classList.add("hidden");
-//   hamburger.classList.remove("fa-bars");
-//   hamburger.classList.remove("fa-solid");
-//   close.classList.remove("hidden");
-//   close.classList.add("fa-xmark");
-//   close.classList.add("fa-solid");
-//   console.log("opener");
-// };
-
-// hamburger.addEventListener("click", opener);
-// close.addEventListener("click", function () {
-//   console.log("close");
-// });
-
 const bars = document.querySelector(".fa-bars");
 const xmark = document.querySelector("#close");
 const menu = document.querySelector("nav ul");
