31890fbae6980a36708de8538e45888100dc03ce
Chieke Emeka <mekus1085@gmail.com>
2023-10-12 21:49:10 +0100

    final touching

--- index.js
+++ index.js
@@ -18,15 +18,18 @@
 //   console.log("close");
 // });
 
-const bars = document.querySelector('.fa-bars');
-const xmark = document.querySelector('#close');
+const bars = document.querySelector(".fa-bars");
+const xmark = document.querySelector("#close");
+const menu = document.querySelector("nav ul");
 
-bars.addEventListener("click", function() {
+bars.addEventListener("click", function () {
   bars.style.display = "none";
-  xmark.style.display = "block"
-})
+  xmark.style.display = "block";
+  menu.style.display = "flex";
+});
 
-xmark.addEventListener("click", function() {
+xmark.addEventListener("click", function () {
   xmark.style.display = "none";
   bars.style.display = "block";
-})
+  menu.style.display = "none";
+});
