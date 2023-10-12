babcceb2dae4d8e5666e5f82afac331ea5490563
Ekwueme Nelson <an.ekwuemem@stu.unizik.edu.ng>
2023-10-12 16:14:07 +0100

    fix: header and typography

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const hamburger = document.querySelector(".fa-bars");
-const show = document.querySelector("ul");
-const close = document.querySelector(".fa-solid");
+// const hamburger = document.querySelector(".fa-bars");
+// const show = document.querySelector("ul");
+// const close = document.querySelector(".fa-solid");
 
-const opener = function () {
-  show.classList.remove("hidden");
-  hamburger.classList.add("hidden");
-  hamburger.classList.remove("fa-bars");
-  hamburger.classList.remove("fa-solid");
-  close.classList.remove("hidden");
-  close.classList.add("fa-xmark");
-  close.classList.add("fa-solid");
-  console.log("opener");
-};
+// const opener = function () {
+//   show.classList.remove("hidden");
+//   hamburger.classList.add("hidden");
+//   hamburger.classList.remove("fa-bars");
+//   hamburger.classList.remove("fa-solid");
+//   close.classList.remove("hidden");
+//   close.classList.add("fa-xmark");
+//   close.classList.add("fa-solid");
+//   console.log("opener");
+// };
 
-hamburger.addEventListener("click", opener);
-close.addEventListener("click", function () {
-  console.log("close");
-});
+// hamburger.addEventListener("click", opener);
+// close.addEventListener("click", function () {
+//   console.log("close");
+// });
+
+const bars = document.querySelector('.fa-bars');
+const xmark = document.querySelector('#close');
+
+bars.addEventListener("click", function() {
+  bars.style.display = "none";
+  xmark.style.display = "block"
+})
+
+xmark.addEventListener("click", function() {
+  xmark.style.display = "none";
+  bars.style.display = "block";
+})
